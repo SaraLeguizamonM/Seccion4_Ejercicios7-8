76568fa36072c9c394e864fd7bc024b94d8776db
Avr1lls <saraleguizamon1234@gmail.com>
2025-12-11 13:26:44 -0500

    Comments in MAIN

--- ejercicios/src/main/java/ejercicio7/Main.java
+++ ejercicios/src/main/java/ejercicio7/Main.java
@@ -1,8 +1,8 @@
-package seccion4;
+package ejercicio7;
 
 public class Main {
     public static void main(String[] args) {
-        String[][] cat = { // ñato
+        String[][] cat = { // ñato - Matrix 11x11
                 {"11","11","11","11","11","11","11","11","11","11","11"},
                 {"11","11","00","11","11","11","00","11","11","11","11"},
                 {"11","11","00","00","11","00","00","11","11","11","11"},
@@ -15,82 +15,88 @@ public class Main {
                 {"11","11","00","00","00","00","00","00","00","11","11"},
                 {"11","11","11","11","11","11","11","11","11","11","11"},
         };
-        System.out.println("Normal - photo");
-        for (int i = 0; i < cat.length; i++) {
+        System.out.println("Normal - photo"); // print original photo
+        System.out.println(" ");
+        for (int i = 0; i < cat.length; i++) { 
             for (int j = 0; j < cat[i].length; j++){
-                System.out.printf(cat[i][j]);
+                System.out.printf(cat[i][j]); // print each element
             }
             System.out.println();
         }
 
 
-        String[][] catRotate = rotate(cat);
+        String[][] catRotate = rotate(cat); // call rotate method
         System.out.println(" ");
         System.out.println(" ");
-        System.out.println("Rotate - photo");
+        System.out.println("Rotate - photo"); // print rotated photo
         System.out.println(" ");
         for (int i = 0; i < catRotate.length; i++) {
             for (int j = 0; j < catRotate[i].length; j++){
-                System.out.printf(catRotate[i][j]);
+                System.out.printf(catRotate[i][j]); // print each element
             }
             System.out.println();
         }
 
-        String[][] catFlip = flip(cat);
+        String[][] catFlip = flip(cat); // call flip method
         System.out.println(" ");
         System.out.println(" ");
-        System.out.println("Flip vertical - photo");
+        System.out.println("Flip vertical - photo"); // print flipped photo
         System.out.println(" ");
         for (int i = 0; i < catFlip.length; i++) {
             for (int j = 0; j < catFlip[i].length; j++){
-                System.out.printf(catFlip[i][j]);
+                System.out.printf(catFlip[i][j]); // print each element
             }
             System.out.println();
         }
 
-        String[][] catTrans = transpose(cat);
+        String[][] catTrans = transpose(cat); // call transpose method
         System.out.println(" ");
         System.out.println(" ");
-        System.out.println("Transpose - photo");
+        System.out.println("Transpose - photo"); // print transposed photo
         System.out.println(" ");
         for (int i = 0; i < catTrans.length; i++) {
             for (int j = 0; j < catTrans[i].length; j++){
-                System.out.printf(catTrans[i][j]);
+                System.out.printf(catTrans[i][j]); // print each element
             }
             System.out.println();
         }
     }
 
-    public static String[][] rotate(String[][] catRotate){
+    // Method to rotate the matrix 90 degrees 
+
+    public static String[][] rotate(String[][] catRotate){ // rotate method
         int n = catRotate.length;
-        String[][] rotate = new String[n][n];
-        for (int i = 0; i < n; i++){
+        String[][] rotate = new String[n][n]; // new matrix to store rotated values
+        for (int i = 0; i < n; i++){ 
             for (int j = 0; j < n; j++){
-                rotate[j*-1+catRotate.length-1][i] = catRotate[i][j];
+                rotate[j*-1+catRotate.length-1][i] = catRotate[i][j]; // rotate logic and assign to new matrix 
             }
         }
         return rotate;
     }
 
-    public static String[][] flip(String[][] catFlip){
+    // Method to flip the matrix vertically
+
+    public static String[][] flip(String[][] catFlip){ // flip method
         int n = catFlip.length;
-        String[][] flip = new String[n][n];
+        String[][] flip = new String[n][n]; // new matrix to store flipped values
         for (int i = 0; i < n; i++){
             for (int j = 0; j < n; j++){
-                flip[i*-1+catFlip.length-1][j] = catFlip[i][j];
+                flip[i*-1+catFlip.length-1][j] = catFlip[i][j]; // flip logic and assign to new matrix - invert rows
 
             }
         }
         return flip;
     }
 
+    // Method to transpose the matrix
 
-    public static String[][] transpose(String[][] catTrans){
+    public static String[][] transpose(String[][] catTrans){ // transpose method
         int n = catTrans.length;
-        String[][] trans = new String[n][n];
+        String[][] trans = new String[n][n]; // new matrix to store transposed values
         for (int i = 0; i < n; i++){
             for (int j = 0; j < n; j++){
-                trans[j][i] = catTrans[i][j];
+                trans[j][i] = catTrans[i][j]; // transpose logic and assign to new matrix - swap rows and columns
             }
         }
         return trans;
